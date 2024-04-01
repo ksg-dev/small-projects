bdd9f3a4bc992e97350aaf1b541537a09f141010
ksg-dev <ksg.dev.data@gmail.com>
2024-04-01 15:53:35 -0500

    band name generator

---
+++ band-name-generator/bandname.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+bandname = city + " " + pet
+
+print("Your band name would be " + bandname)
