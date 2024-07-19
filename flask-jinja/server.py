e632aa7c2010f36fe3173b92b10ec9e78c44bddd
ksg-dev <ksg.dev.data@gmail.com>
2024-07-19 12:46:20 -0500

    dynamic footer

--- flask-jinja/server.py
+++ flask-jinja/server.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 import random
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     random_number = random.randint(1,10)
-    return render_template("index.html", num=random_number)
+    current_year = date.today().year
+    me = "Sonni"
+    return render_template("index.html", num=random_number, current_year=current_year, me=me)
 
 
 if __name__ == "__main__":
